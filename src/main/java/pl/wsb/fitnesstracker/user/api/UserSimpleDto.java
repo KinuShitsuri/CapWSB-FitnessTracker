084838e3064026f18235392b2de3139a50684b23
Marcin Ciućmański <>
2025-05-11 12:08:22 +0200

    lab1 - add endpoint to find by id

--- src/main/java/pl/wsb/fitnesstracker/user/api/UserSimpleDto.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/UserSimpleDto.java
@@ -1,6 +1,5 @@
 package pl.wsb.fitnesstracker.user.api;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.annotation.Nullable;
 
 public record UserSimpleDto(@Nullable Long id, String firstName, String lastName){
--- src/main/java/pl/wsb/fitnesstracker/user/internal/UserMapper.java
+++ src/main/java/pl/wsb/fitnesstracker/user/internal/UserMapper.java
@@ -2,6 +2,7 @@ package pl.wsb.fitnesstracker.user.internal;
 
 import org.springframework.stereotype.Component;
 import pl.wsb.fitnesstracker.user.api.User;
+import pl.wsb.fitnesstracker.user.api.UserSimpleDto;
 
 @Component
 class UserMapper {
